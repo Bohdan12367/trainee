d42bfabe6cfdd884812bec0b36f75c8357a0a9b6
Bohdan Lukasevych <>
2022-11-08 10:28:08 +0200

    test

--- force-app/main/default/lwc/inputCity/inputCity.js
+++ force-app/main/default/lwc/inputCity/inputCity.js
@@ -1,7 +1,5 @@
 import {LightningElement,track,wire} from 'lwc';
-
 import sendWeather from '@salesforce/apex/WeatherRest.weatherReturn';
-//import getW from '@salesforce/apex/WeatherRest.getWeather';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class InputCity extends LightningElement {
@@ -11,13 +9,9 @@ export default class InputCity extends LightningElement {
 
     getCity () {
          sendWeather ({city: this.searchKey})
-            // .then(result=>{
-            //     return getW({city:this.searchKey});
-            //})
             .then(result=>{
                 this.weather = result;
                 this.imageURL = result.Image__c;
-                console.log('success');
             })
             .catch(error =>
                 this.dispatchEvent (
@@ -31,6 +25,5 @@ export default class InputCity extends LightningElement {
     }
     handleChange(event) {
         this.searchKey = event.target.value;
-        console.log(this.searchKey);
     }
 }
